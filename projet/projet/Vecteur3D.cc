394241430726e5cdd76341815338d3880a63502b
Adrien Dürst <adrien.durst@epfl.ch>
2019-03-05 21:54:37 +0100

    Comment
    Descriptions des méthodes de Vecteur3D

--- projet/projet/Vecteur3D.cc
+++ projet/projet/Vecteur3D.cc
@@ -2,11 +2,13 @@
 #include <cmath>
 namespace std;
 
+//affiche() : affiche les composantes x, y, z d'un vecteur sur la console
 void Vecteur3D::affiche() const {
 		
 		cout << x << " " << y << " " << z << endl;
 	}
-	
+
+//compare() : Compare l'instance coordonnée par coordonnée avec un vecteur passé en paramètre 
 bool Vecteur3D::compare(Vecteur3D Acomparer) const
 	{
 		if ((x == Acomparer.getx()) and (y ==  Acomparer.gety()) and (z == Acomparer.getz()))
@@ -21,14 +23,15 @@ bool Vecteur3D::compare(Vecteur3D Acomparer) const
 	}
 }
 
+/*Setter !!A REMPLACER!!*/
 void Vecteur3D::set(double xx, double yy, double zz) 
 {
-	
 	x = xx;
 	y = yy;
 	z = zz;
 }
 
+//addition() : Additionne l'instance avec un vecteur passé en paramètre
 Vecteur3D Vecteur3D::addition(Vecteur3D autre) const
 {	
 	Vecteur3D nouveau;
@@ -37,6 +40,7 @@ Vecteur3D Vecteur3D::addition(Vecteur3D autre) const
 	return nouveau;
 }
 
+//soustraction() : Soustrait l'instance avec un vecteur passé en paramètre
 Vecteur3D Vecteur3D::soustraction(Vecteur3D autre) const
 {
 	Vecteur3D nouveau;
@@ -45,6 +49,7 @@ Vecteur3D Vecteur3D::soustraction(Vecteur3D autre) const
 	return nouveau;
 }
 
+//oppose() : Multiplie l'instance par (-1)
 Vecteur3D Vecteur3D::oppose () const 
 { 	
 	Vecteur3D nouveau;
@@ -53,6 +58,7 @@ Vecteur3D Vecteur3D::oppose () const
 	return nouveau;
 }
 
+//mult() : Multiplie l'instance par un scalaire
 Vecteur3D Vecteur3D::mult (double scalaire) const
 {
 	Vecteur3D nouveau;
@@ -61,6 +67,7 @@ Vecteur3D Vecteur3D::mult (double scalaire) const
 	return nouveau;
 }
 
+//prod_scal() : Effectue un produit scalaire entre l'instance et un vecteur passé en paramètre
 double Vecteur3D::prod_scal(Vecteur3D autre) const
 {
 	double produit_scalaire;
@@ -69,6 +76,7 @@ double Vecteur3D::prod_scal(Vecteur3D autre) const
 	return produit_scalaire;
 }
 
+//prod_vect() : Effectue un produit vectoriel entre l'instance et un vecteur passé en paramètre
 Vecteur3D Vecteur3D::prod_vect(Vecteur3D autre) const
 { 
 	Vecteur3D nouveau;
@@ -77,6 +85,7 @@ Vecteur3D Vecteur3D::prod_vect(Vecteur3D autre) const
 	return nouveau;
 }
 
+//norme() : Calcule la norme de l'instance
 double Vecteur3D::norme() const
 {
 	double norm;
@@ -85,6 +94,7 @@ double Vecteur3D::norme() const
 	return norm;
 }
 
+//norme2() : Calcule le carré de la norme de l'instance
 double Vecteur3D::norme2() const
 { 
 	double norme_carre;
@@ -93,6 +103,7 @@ double Vecteur3D::norme2() const
 	return norme_carre;
 }
 
+//unitaire() : Retourne le vecteur unitaire de même direction que l'instance
 Vecteur3D Vecteur3D::unitaire() const 
 {
 	Vecteur3D nouveau;
@@ -101,17 +112,11 @@ Vecteur3D Vecteur3D::unitaire() const
 	return nouveau;
 }
 
+//prod_mixte() : Effectue un produit mixte entre l'instance et deux vecteurs passés en paramètre
 double Vecteur3D::prod_mixte(Vecteur3D V1, Vecteur3D V2) const 
 {
 	double produit_mixte;
 	produit_mixte = prod_scal(V1.prod_vect(V2));
 	
 	return produit_mixte;
-}
-
-int main ()
-{
-	Vecteur3D vecteur;
-	vecteur.set(2.0,1.0,21);
-	cout<<vecteur.getx()<<", "<<vecteur.gety()<<", "<<vecteur.getz();
 }
