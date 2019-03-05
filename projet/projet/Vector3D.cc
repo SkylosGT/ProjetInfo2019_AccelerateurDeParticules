3e67f1716e4daf5cbe4905eea7f9950eef4769c9
josephine potdevin <josephinepotdevin@gmail.com>
2019-03-05 18:54:55 +0100

    Update Vector3D.cc
    Quelques remarques en plus

--- projet/projet/Vector3D.cc
+++ projet/projet/Vector3D.cc
@@ -172,4 +172,5 @@ double Vecteur3D::prod_mixte(Vecteur3D V1, Vecteur3D V2) const
 int main ()
 {
 	// Est-ce qu'on met quelque chose dans le main ou pas?? A chaque fonction faite je l'ai testée 
+	// Quels sont les fonctions à mettre dans public et private ??
 }
