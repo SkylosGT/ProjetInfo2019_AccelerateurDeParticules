82481380e10de86e634d4ce9a7c265105665d12e
josephine potdevin <josephinepotdevin@gmail.com>
2019-03-07 10:06:37 +0100

    Constructeur Vecteur3D.h
    Ajouts des onstructeur

--- projet/projet/testVecteur.cc
+++ projet/projet/testVecteur.cc
@@ -1,6 +1,4 @@
-#include <iostream>
 #include "Vecteur3D.h"
-using namespace std;
 
 /*Boucle principale
 pour le moment il ne s'agit que du test de la classe vecteur3D*/
@@ -9,10 +7,8 @@ int main ()
 	const double testX=2.4;
 	const double testY= 7.1;
 	const double testZ= 3.7;
-	Vecteur3D testVecteur1;
-	Vecteur3D testVecteur2;
-	testVecteur1.set(testX, testY, testZ);
-	testVecteur2.set(testX, testY, testZ);
+	Vecteur3D testVecteur1(testX, testY, testZ);
+	Vecteur3D testVecteur2(testX, testY, testZ);
 	testVecteur1.affiche();
 	testVecteur2.affiche();
 	testVecteur1.addition(testVecteur2).affiche();
