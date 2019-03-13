074620c487192c8fea0359ccba6588dec8a11f81
Adrien Dürst <adrien.durst@outlook.com>
2019-03-13 21:59:37 +0100

    Utilisation des constantes dans la définition

--- projet/programme/Particule.cc
+++ projet/programme/Particule.cc
@@ -1,6 +1,9 @@
 #include "Particule.h"
+#include "ConstantesPhysiques.h"
 #include <cmath>
  
+using  namespace ConstantesPhysiques;
+
 /*!!Changer de place */ 
 const double c(1);
 Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
@@ -10,16 +13,16 @@ Particule::~Particule()
 {
 }
 
+//utilisation de pow() pour une meilleure compréhension
 double Particule::scalNormeVitesse(){
     double scal_normeP= vec_p.norme();
-    //utilisation de pow() pour une meilleure compréhension
-    return (scal_normeP)/(sqrt(pow(scal_m, 2)+pow((scal_normeP/ c),2))); //!!c temporaire
+    return (scal_normeP)/(sqrt(pow(scal_m, 2)+pow((scal_normeP/ scal_c),2)));
 }
 
 double Particule::scalEnergie(){
-    return scalFacteurGamma()*scal_m*pow(c, 2); //!!c temporaire
+    return scalFacteurGamma()*scal_m*pow(scal_c, 2); 
 }
 
 double Particule::scalFacteurGamma(){
-    return 1/sqrt(1-pow(scalNormeVitesse()/c,2)); //!!c temporaire
+    return 1/sqrt(1-pow(scalNormeVitesse()/scal_c,2));
 }
