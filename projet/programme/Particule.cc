194cb39356304f77e37b5b900d972286eb0cb031
Adrien Dürst <adrien.durst@outlook.com>
2019-03-13 21:35:01 +0100

    Implémentation du constructeur

--- projet/programme/Particule.cc
+++ projet/programme/Particule.cc
@@ -1,9 +1,17 @@
 #include "Particule.h"
+#include <cmath>
 
-Particule::Particule(/* args */)
-{
-}
+Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
+:vec_r(_r), vec_p(_p), scal_m(_m), scal_q(_q) {}
 
 Particule::~Particule()
 {
+}
+
+double Particule::scalEnergy(){
+
+}
+
+double Particule::scalGammaFactor(){
+
 }
--- projet/programme/Particule.h
+++ projet/programme/Particule.h
@@ -1,23 +1,24 @@
 #include "Vecteur3D.h"
 
 /*Header de la classe particule
-  unités GeV*/
+  unités SI*/
 
 class Particule
 {
 private:
     Vecteur3D vec_r; //Position de la particule dans R3
-    /*Vecteur vitesse normé??*/
     Vecteur3D vec_p; //Quantité de mouvement de la particule dans R3
     double scal_m; //Masse de la particule
     double scal_q; //Charge de la particule
-    
-    /*Temporaire*/
-    double scal_E; //Energie de la particule  
-    double scal_gamma; //Facteur gamma de la particule
 
 public:
-    Particule(Vecteur3D/*position*/, Vecteur3D, double/*masse*/, double, double);
+    //Constructeur
+    Particule(/*Position*/Vecteur3D, /*Quantité de mouvement*/Vecteur3D, /*Masse*/double, /*Charge*/double);
+    //Déstructeur
     ~Particule();
+    //Fonction Energie
+    double scalEnergy();
+    //Fonction Facteur Gamma
+    double scalGammaFactor();
 };
 
