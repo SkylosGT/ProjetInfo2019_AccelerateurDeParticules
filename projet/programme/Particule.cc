75bfdb4d5717c466a573c054f7b03a849354819b
Adrien Dürst <adrien.durst@outlook.com>
2019-03-13 21:44:45 +0100

    implémentation scalNormeVitesse

--- projet/programme/Particule.cc
+++ projet/programme/Particule.cc
@@ -1,6 +1,7 @@
 #include "Particule.h"
 #include <cmath>
-
+/*!!Changer de place */ 
+const double c(1);
 Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
 :vec_r(_r), vec_p(_p), scal_m(_m), scal_q(_q) {}
 
@@ -9,7 +10,8 @@ Particule::~Particule()
 }
 
 double Particule::scalNormeVitesse(){
-    
+    double scal_normeP= vec_p.norme();
+    return (scal_normeP)/(sqrt(pow(scal_m, 2)+pow((scal_normeP/ c),2))); //!!c temporaire
 }
 
 double Particule::scalEnergie(){
