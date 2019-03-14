ac75c1d46efbc397d97ba3485916551699ba476d
Adrien Dürst <adrien.durst@outlook.com>
2019-03-14 17:11:28 +0100

    programme testVecteur

--- projet/programme/testParticule.cc
+++ projet/programme/testParticule.cc
@@ -1,11 +1,21 @@
 #include <iostream>
 #include "Particule.h"
+#include "Vecteur3D.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    Particule p;
+    Vecteur3D r;
+    r.set(0,0,0);
+    Vecteur3D v;
+    v.set(1,1,1);
+    double m(10);
+    double q(5);
+    Vecteur3D p;
+    p.set(3,3,3);
+
+    Particule p(r,p,m,q);
     p.affiche();
     return 0;
 }
