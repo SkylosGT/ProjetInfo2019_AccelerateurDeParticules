9c38e75f23cdb479c07ba57f8bca018243722d97
Adrien Dürst <adrien.durst@outlook.com>
2019-03-14 11:07:16 +0100

    méthode energie, facteur gamma

--- projet/programme/Particule.cc
+++ projet/programme/Particule.cc
@@ -1,35 +1,21 @@
 #include "Particule.h"
 #include "ConstantesPhysiques.h"
 #include <cmath>
- 
 using  namespace ConstantesPhysiques;
+
 //Constructeur par vecteur quantité de mouvement en Gev
 Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
 :vec_r(_r), scal_m(_m), scal_q(_q) 
 {
-
+    vec_v=_p.mult(scal_c/sqrt((_m*_m)+_p.norme2()));
 }
 
 //Constructeur par energie en GeV et direction vectorielle
 Particule::Particule(Vecteur3D _r, Vecteur3D _vitesseUnitaire, double _energie, double _m, double _q)
-:vec_r(_r), scal_m(_m), scal_q(_q) {}
+:vec_r(_r), scal_m(_m), scal_q(_q) 
+{
+    vec_v=_vitesseUnitaire.mult(scal_c*sqrt(1-(scal_m*scal_m)/(_energie*_energie)));
+}
 
 Particule::~Particule(){}
 
-Vecteur3D Particule::toVec_pSI(Vecteur3D _p){
-    return _p.mult()
-}
-
-//utilisation de pow() pour une meilleure compréhension
-double Particule::scalNormeVitesse(){
-    double scal_normeP= vec_p.norme();
-    return (scal_normeP)/(sqrt(pow(scal_m, 2)+pow((scal_normeP/ scal_c),2)));
-}
-
-double Particule::scalEnergie(){
-    return scalFacteurGamma()*scal_m*pow(scal_c, 2); 
-}
-
-double Particule::scalFacteurGamma(){
-    return 1/sqrt(1-pow(scalNormeVitesse()/scal_c,2));
-}
--- projet/programme/Particule.h
+++ projet/programme/Particule.h
@@ -1,12 +1,12 @@
 #include "Vecteur3D.h"
 
 /*Header de la classe particule
-  unités SI*/
+  unités GeV*/
 
 class Particule
 {
 private:
-    Vecteur3D vec_r; //Position de la particule dans R3
+    Vecteur3D vec_r; //Position de la particule dans R3 
     Vecteur3D vec_v; //Quantité de mouvement de la particule dans R3
     double scal_m; //Masse de la particule
     double scal_q; //Charge de la particule
@@ -14,22 +14,15 @@ private:
 public:
     //Constructeur par vecteur quantité de mouvement en Gev
     Particule(Vecteur3D /*Position*/, Vecteur3D /*Quantité de mouvement*/, double/*Masse*/, double/*Charge*/);
+
     //Constructeur par energie en GeV et direction vectorielle
     Particule(Vecteur3D/*Position*/, Vecteur3D /*Direction Vectorielle*/, double /*Energie*/, double/*Masse*/, double /*Charge*/ );
+    
     //Déstructeur
     ~Particule();
-  
-    //Fonction transforme quantité de mouvement en GeV à quantité de mouvement SI
-    Vecteur3D toVec_pSI(Vecteur3D/*Quantité de mouvement*/);
-    
-    //Fonction transforme quantité de mouvement en GeV à quantité de mouvement SI
-    double toScalEnergieSI(double /*Energie*/);
 
-    //Fonction norme de la vitesse
-    double scalNormeVitesse();
-    //Fonction Energie
-    double scalEnergie();
-    //Fonction Facteur Gamma
-    double scalFacteurGamma();
+    double Energie();
+
+    double FacteurGamma();
 };
 
