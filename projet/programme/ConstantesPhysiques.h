d632bcbe142a864d58b81ae2a8224735de2952d0
Adrien Dürst <adrien.durst@outlook.com>
2019-03-20 20:17:33 +0100

    Update power

--- projet/programme/ConstantesPhysiques.h
+++ projet/programme/ConstantesPhysiques.h
@@ -2,5 +2,5 @@
 
 namespace ConstantesPhysiques {
     const double const_c=299792458; //Scalaire constant vitesse de la lumière dans le vide
-    const double const_e=1.60217653*pow(10, -19);
+    const double const_e=1.60217653e-19;
 }
--- projet/programme/testParticule.cc
+++ projet/programme/testParticule.cc
@@ -9,15 +9,15 @@ using namespace ConstantesPhysiques;
 int main()
 {
     Vecteur3D _r1(3.00984, -0.391837, 0), _r2(2.99016, -0.391837, 0);
-    Vecteur3D _v1(-1,-1,0);
+    Vecteur3D _v1(-210200, -2.64754e+8, 0), _v2(210200, -2.64754e+8, 0);
     double _E(2);
     double _m(0.938272);
     double _q(const_e);
-    Particule p1(_r1,_v1,_E,_m,_q), p2(_r2,_v1,_E,_m,_q);
+    Particule p1(_r1,_v1,_E,_m,_q), p2(_r2,_v2,_E,_m,_q);
     cout<<"Création de deux particules :";
     cout<<p1<<endl<<p2<<endl;  
-    Vecteur3D _B(0,0,7);
-    double _dt (pow(10,-11));
-    p1.ajouteForceMagnetique(_B, _dt);
+    //Vecteur3D _B(0,0,7);
+    //double _dt (1e-11);
+    //p1.ajouteForceMagnetique(_B, _dt);
     return 0;
 }
