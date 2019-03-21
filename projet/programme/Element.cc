fe759fadbf0821dfda2eb3e09a0d84fbb93cc8fa
Adrien Dürst <adrien.durst@outlook.com>
2019-03-21 11:19:18 +0100

    Définition des méthodes de Element

--- projet/programme/Element.cc
+++ projet/programme/Element.cc
@@ -5,5 +5,12 @@ Element::Element(Vecteur3D _re, Vecteur3D _rs, double _Re, Element const& _suiva
 {}
 
 Element::~Element()
-{
+{}
+
+bool Element::heurte_bord(Particule const& _p) const{
+    return true;
+}
+
+bool Element::passe_au_suivant(Particule const& _p) const{
+    return true;
 }
