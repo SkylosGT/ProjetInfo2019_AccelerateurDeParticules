4129876a2f72f303b33f5e47b619cbd135a3ba15
Adrien Dürst <adrien.durst@epfl.ch>
2019-03-23 15:51:38 +0100

    Mise en page Particule.cc

--- projet/programme/Particule.cc
+++ projet/programme/Particule.cc
@@ -1,26 +1,31 @@
-#include "Particule.h"
-#include "ConstantesPhysiques.h"
 #include <iostream>
 #include <cmath>
+
+#include "Particule.h"
+#include "ConstantesPhysiques.h"
 
 using  namespace ConstantesPhysiques;
 using namespace std;
 
-//Constructeur par vecteur quantité de mouvement en Gev
-Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
-:vec_r(_r), scal_m(_m), scal_q(_q) 
-{
-    vec_v=_p*(const_c/sqrt((_m*_m)+_p.norme2()));
-    vec_f=*new Vecteur3D();
-}
+//DEFINITION DES METHODES DE LA CLASSE PARTICULE ET DE LA SURCHARGE DE SES OPERATEURS
 
-//Constructeur par energie en GeV et direction vectorielle
+//METHODE PRIVEE DE LA CLASSE PARTICULE
+double Particule::transformMassGeVToKg(){
+    return (const_e*1e+9*scal_m)/(const_c*const_c);}
+
+double Particule::calculateDeviationAngle(double _dt){
+    return asin((_dt*vec_f.norme())/(2*FacteurGamma()*massGeVToKg()*vec_v.norme()));}
+
+//METHODES PUBLIQUES DE LA CLASSE PARTICULE
+Particule::Particule(Vecteur3D _r, Vecteur3D _p, double _m, double _q)
+:vec_r(_r), scal_m(_m), scal_q(_q) {
+    vec_v=_p*(const_c/sqrt((_m*_m)+_p.norme2()));
+    vec_f=*new Vecteur3D();}
+
 Particule::Particule(Vecteur3D _r, Vecteur3D _vitessedirective, double _energie, double _m, double _q)
-:vec_r(_r), scal_m(_m), scal_q(_q) 
-{
+:vec_r(_r), scal_m(_m), scal_q(_q) {
     vec_v=(~_vitessedirective)*(const_c*sqrt(1-(scal_m*scal_m)/(_energie*_energie)));
-    vec_f=*new Vecteur3D();
-}
+    vec_f=*new Vecteur3D();}
 
 double Particule::Energie() const
 {return FacteurGamma()*scal_m;}
@@ -33,15 +38,12 @@ void Particule::ajouteForceMagnetique(Vecteur3D _B, double _dt){
     cout<<" F0 = "<<vec_f<<endl;
     vec_f.rotation((vec_v^vec_f), deviationAngle(_dt));
     cout<<" F1 = "<<vec_f<<endl;
-    cout<<" Angle : "<<deviationAngle(_dt)<<endl;
-}
-
+    cout<<" Angle : "<<deviationAngle(_dt)<<endl;}
 
 void Particule::bouger(double _dt){
     vec_v=vec_v+_dt*(1/(FacteurGamma()*massGeVToKg()))*vec_f;
     vec_r=vec_r+(vec_v*(_dt));
-    vec_f=*new Vecteur3D();
-}
+    vec_f=*new Vecteur3D();}
 
 ostream& Particule::affiche(ostream& sortie) const{
     return sortie<<"Une particule :"<<endl<<
@@ -51,19 +53,8 @@ ostream& Particule::affiche(ostream& sortie) const{
     "   enregie (GeV) :"<<Energie()<<endl<<
     "   masse (Gev)/c^2 :"<<scal_m<<endl<<
     "   charge :"<<scal_q<<endl<<
-    "   force :"<<vec_f<<endl;
-}
+    "   force :"<<vec_f<<endl;}
 
-double Particule::massGeVToKg(){
-    return (const_e*1e+9*scal_m)/(const_c*const_c);
-}
-
-double Particule::deviationAngle(double _dt){
-    return asin((_dt*vec_f.norme())/(2*FacteurGamma()*massGeVToKg()*vec_v.norme()));
-}
-
-Particule::~Particule(){}
-
-//Attention passage d'un objet de manière non constante non constant à changer
+//OPERATEURS EXTERNES A LA CLASSE PARTICULE UTILISANT LES METHODES DE LA CLASSE
 ostream& operator<<(ostream& sortie, Particule const& P){
    return P.affiche(sortie);}
