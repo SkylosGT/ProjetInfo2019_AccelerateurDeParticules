5ea4a9c70abb082b56d0b3d3b9cdafc5aac5c29f
Adrien Dürst <adrien.durst@outlook.com>
2019-03-24 17:02:16 +0100

    Update classe élément

--- projet/programme/Element.cc
+++ projet/programme/Element.cc
@@ -1,16 +1,21 @@
 #include "Element.h"
+#include <iostream>
 
-Element::Element(Vecteur3D _re, Vecteur3D _rs, double _Re, Element const& _suivant, Vecteur3D _B)
-:vec_re(_re), vec_rs(_rs), scal_Re(_Re), elem_suivant(_suivant), vect_B(_B)
+using namespace std;
+
+Element::Element(Vecteur3D _re, Vecteur3D _rs, double _Re, Element const& _suivant)
+:vec_re(_re), vec_rs(_rs), scal_Re(_Re), elem_suivant(_suivant)
 {}
 
 Element::~Element()
 {}
 
 bool Element::heurte_bord(Particule const& _p) const{
+    cout<<"Heurte_bord"<<endl;
     return false;
 }
 
 bool Element::passe_au_suivant(Particule const& _p) const{
+    cout<<"Heurte_bord"<<endl;
     return false;
 }
--- projet/programme/Element.h
+++ projet/programme/Element.h
@@ -8,11 +8,11 @@ private:
     Vecteur3D vec_rs;
     double scal_Re;
     Element const& elem_suivant;
-    Vecteur3D vect_B;
 
 public:
-    Element(Vecteur3D, Vecteur3D, double, Element const&, Vecteur3D);
+    Element(Vecteur3D, Vecteur3D, double, Element const&);
     ~Element();
+    void champsMagnetique(Vecteur3D) const;
     bool heurte_bord(Particule const&) const;
     bool passe_au_suivant(Particule const&) const;
 };
