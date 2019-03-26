473de3cfa1b3fb21bbbc48f2981e355334549bc3
Adrien Dürst <adrien.durst@outlook.com>
2019-03-26 18:32:34 +0100

    Update champsmagnetique

--- projet/programme/Dipole.cc
+++ projet/programme/Dipole.cc
@@ -1,7 +1,15 @@
 #include "Dipole.h"
+#include "ConstantesPhysiques.h"
+
+using namespace ConstantesPhysiques;
+
 
 Dipole::Dipole(Vecteur3D _re, Vecteur3D _rs, double _Re, Element const& _suivant, double _k, double _Bz)
 :ElementCourbe(_re, _rs, _Re, _suivant, _k), scal_Bz(_Bz) {}
 
 Dipole::~Dipole()
 {}
+
+Vecteur3D Dipole::champMagnetique(Vecteur3D const& _r) const {
+    return scal_Bz*_r;
+}
