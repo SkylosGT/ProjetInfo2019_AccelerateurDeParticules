f08dd45f194123dd4cbd4ab039910a199fec900b
josephine potdevin <josephinepotdevin@gmail.com>
2019-03-28 08:19:55 +0100

    Essay de faire qqchose

--- projet/programme/Accelerateur.cc
+++ projet/programme/Accelerateur.cc
@@ -1,1 +1,7 @@
+#include <Accelerateur.h>
+#include <vector>
 
+Accelerateur::Accelerateur() 
+	: TabElement(0), TabParticule(0) {}
+	
+vector Accelerateur::getTabElement() const {return TabElement;}
--- projet/programme/Accelerateur.h
+++ projet/programme/Accelerateur.h
@@ -1,1 +1,31 @@
+#pragma once
+#include <iostream>
+#include "ElementCourbe.h"
+#include "ElementDroit.h"
+#include "Dipole.h"
+#include "Particule.h"
+#include <vector>
 
+//En tête de la classe Accelerateur avec descriptions de ses méthodes. Corps dans "Particule.h"
+class Accelerateur {
+	
+	private : 
+	//Une structure contenant tout les éléments d'un accélérateur de particule sous forme de tableaux (Elements droits, courbes et les dipôles)
+		struc CollectionElement {
+			vector <ElementCourbe> CollectionElementCourbe;
+			vector <ElementDroit> CollectionElementDroit;
+			vector <Dipole> CollectionDipole; };
+	
+	//Un tableau de particules (pas besoin de structure car les particules n'ont pas de sous-classes, de spécificationss)
+		vector <Particule> CollectionParticule;
+		
+	
+	public :
+		Accelerateur();
+		
+		vector getCollectionElement() const;
+		vector getCollectionParticule() const;
+		
+		std::ostream& affiche(std::ostream&) const;
+	
+};
