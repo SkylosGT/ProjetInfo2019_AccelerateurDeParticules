e8335509589dd1539bb4ae351d80c022ee89f54f
Adrien Dürst <adrien.durst@outlook.com>
2019-03-30 14:55:41 +0100

    Mise en page Element.cc

--- projet/programme/Element.cc
+++ projet/programme/Element.cc
@@ -8,13 +8,18 @@ using namespace std;
 
 //DEFINITION DES METHODES DE LA CLASSE ELEMENT ET SURCHARGE DE SES OPERATEURS
 
+//METHODES PUBLIQUES DE LA CLASSE PARTICULE
 Element::Element(Vecteur3D _re, Vecteur3D _rs, double _Re, Element* _suivant)
-:vec_re(_re), vec_rs(_rs), scal_Re(_Re), elem_suivant(_suivant)
-{}
+:vec_re(_re), vec_rs(_rs), scal_Re(_Re), elem_suivant(_suivant) {}
 
-Element::~Element()
-{}
+Element::~Element() {}
 
 bool Element::passe_au_suivant(Particule const& _p) const{
     if (vec_e3*(_p.position()^vec_rs)>0) {return true;}
     else{false;}}
+
+void Element::attache_element_suivant(Element* _suivant){
+    elem_suivant=_suivant;}
+
+std::ostream& operator<<(std::ostream& sortie, Element const& E){
+    return E.affiche(sortie);}
