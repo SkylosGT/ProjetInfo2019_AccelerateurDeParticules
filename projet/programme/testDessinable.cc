5cb68435281545bec6ce5ae922172046433f3de4
Adrien Dürst <adrien.durst@epfl.ch>
2019-04-04 16:37:35 +0200

    Ajout d'un dipole au test dessin

--- projet/programme/testDessinable.cc
+++ projet/programme/testDessinable.cc
@@ -2,6 +2,7 @@
 #include "SupportConsole.h"
 #include "Contenu.h"
 #include "Particule.h"
+#include "Dipole.h"
 using namespace std;
 
 int main()
@@ -29,13 +30,30 @@ int main()
   //Charge de p
   double _q(1);
 
+   //Position d'entrée de D
+    Vecteur3D _re1(1,0,0);
+
+    //Position de sortie de D
+    Vecteur3D _rs1(0,-1,0);
+    
+    //Rayon de la chambre à vide de D
+    double _Re1(0.1);
+
+    //Rayon de courbure de D
+    double _k(1);
+
+    //Norme du champ magnetique de D 
+    double _Bz(7);
+
+
   Particule p(_r1, _v1, _E, _m, _q, &ecran);
 
-  
+  Dipole d(_re1, _rs1, _Re1, _k,_Bz, &ecran);
 
   // Nous dessinons notre contenu
   c.dessine();
   p.dessine();
+  d.dessine();
 
   return 0;
 }
