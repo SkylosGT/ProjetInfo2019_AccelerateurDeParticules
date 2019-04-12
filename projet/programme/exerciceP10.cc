aa15d13cce7c4c36f1e10a7e04cff5cc7e1d06b3
Adrien Dürst <adrien.durst@outlook.com>
2019-04-12 16:22:12 +0200

    update exercicep10

--- projet/programme/exerciceP10.cc
+++ projet/programme/exerciceP10.cc
@@ -16,12 +16,47 @@ int main()
 
 	//Un accélérateur par défauts (vide : aucun élément, aucune particule)
 	Accelerateur _accelerateur(&ecran);
+	
+	/*Attributs physiques de deux particules P1 et P2*/
+	//Positions de P1 et P2
+    Vecteur3D _r1(3.01, 0, 0), _r2(2.99, 0, 0);
+
+    //Vitesses de P1 et P2
+    Vecteur3D _v1(0, -2.64754e+8, 0), _v2(0, -2.64754e+8, 0);
+
+    //Energie de P1 et P2
+    double _E(2);
+
+    //Masse de P1 et P2
+    double _m(0.938272);
+
+    //Charge de P1 et P2
+    double _q(const_e);
 
     /*Deux particules P1 et P2*/
-    Particule p1((*new Vecteur3D(3.01,0,0)), (*new Vecteur3D(0,-2.64754e+08,0)), 2, 0.938272, const_e);
-    Particule p2((*new Vecteur3D(2.99,0,0)), (*new Vecteur3D(0,-2.64754e+08,0)), 2, 0.938272, const_e);
-
+    Particule p1(_r1, _v1, _E, _m, _q), p2(_r2,_v2,_E,_m,_q);
     
+    //Test de l'ajout des éléments dans l'accélérateur
+    _accelerateur.ajoutElement(&D);
+    _accelerateur.ajoutElement(&S);
+    _accelerateur.ajoutElement(&Q);
+
+    //Test de l'ajout des deux particules dans l'accélérateur
+    _accelerateur.ajoutParticule(p1);
+    _accelerateur.ajoutParticule(p2);
+
+    //Affichage de tout les éléments et particules que contient l'accélérateur
+    cout <<"Après ajout d'un dipole, d'un quadrupole, d'une section droite et de deux particules dans l'accélérateur : " << endl;
+    _accelerateur.dessine();
+    
+    //Test de la suppression des éléments et particules de l'accélérateur
+    _accelerateur.supprCollectionParticule();
+    _accelerateur.supprCollectionElement();
+
+    //Réaffichage de l'accélérateur pour tester que l'accélérateur ne contient aucun élément ni aucune particule
+    cout << "On supprime tous les éléments et toutes les particules de l'accélérateur:"<< endl; cout << endl;
+    _accelerateur.dessine();
+
 
     return 0;
 }
