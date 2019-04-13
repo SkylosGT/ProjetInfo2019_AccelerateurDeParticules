1ede481f10082812803f8519d9243aab5c126da1
josephine potdevin <josephinepotdevin@gmail.com>
2019-04-13 17:04:45 +0200

    Redéfintion de la fonction dessine de accélérateur

--- projet/programme/SupportConsole.cc
+++ projet/programme/SupportConsole.cc
@@ -16,4 +16,32 @@ void SupportConsole::dessine(Quadrupole const& _q){_q.affiche(flot);}
 
 void SupportConsole::dessine(SectionDroite const& _s){_s.affiche(flot);}
 
-void SupportConsole::dessine(Accelerateur const& _a){_a.affiche(flot);}
+void SupportConsole::dessine(Accelerateur const& _a){
+	if (_a.getCollectionEl().size() > 0) {
+		if (_a.getCollectionEl().size() == 1) {
+			cout << "L'accélérateur est constitué de l'élément suivant : " << endl;
+					(_a.getCollectionEl()[0])->dessine();
+					cout << endl;
+				}else{
+					cout << "L'accélérateur est constitué des " <<_a.getCollectionEl().size() <<" éléments suivantes :"<<endl;
+					for (auto element : _a.getCollectionEl()) {
+						element->dessine();}}
+						cout << endl; } else {
+							cout << "L'accélérateur n'est constitué d'aucun élément."<<endl;
+						cout << endl;}
+	
+	if (_a.getCollectionPa().size() > 0) { 
+		if (_a.getCollectionPa().size() == 1) {
+			cout << "L'accélérateur contient la particule suivante : " << endl;
+					(_a.getCollectionPa()[0]).dessine();
+					cout << endl;
+				}else{
+					cout << "L'accélérateur contient les " <<_a.getCollectionPa().size() <<" particules suivantes :"<<endl;
+					for (auto particule : _a.getCollectionPa()) {
+						particule.dessine();
+						cout << endl;}}
+						}else{
+							cout << "L'accélérateur ne contient aucune particule."<< endl;
+							cout << endl;
+	}
+}
