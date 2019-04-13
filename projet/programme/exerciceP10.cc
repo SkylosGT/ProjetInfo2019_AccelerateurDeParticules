c26a4e9fe270b3fba6c8c64f301e04c8262dbe99
Adrien Dürst <adrien.durst@outlook.com>
2019-04-13 16:50:41 +0200

    particule 2

--- projet/programme/exerciceP10.cc
+++ projet/programme/exerciceP10.cc
@@ -78,7 +78,8 @@ int main()
 
     //Test de l'ajout des deux particules dans l'accélérateur
     _accelerateur.ajoutParticule(&p1);
-    //_accelerateur.ajoutParticule(p2);
+    _accelerateur.ajoutParticule(&p2);
+    
 for(size_t i = 0; i < 20; i++)
 {
 
