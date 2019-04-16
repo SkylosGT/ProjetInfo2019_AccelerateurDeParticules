a8fae753b8c1075697da4901f7fea5d1aae03940
josephine potdevin <josephinepotdevin@gmail.com>
2019-04-16 14:45:51 +0200

    Debut de definition du constructeur

--- projet/programme/Faisceau.cc
+++ projet/programme/Faisceau.cc
@@ -1,1 +1,9 @@
+#include "Faisceau.h"
+
+using namespace std;
+
+Faisceau::Faisceau(Particule _ref, long int _nb, double _coef, SupportADessin* _support)
+: Dessinable(_support), reference(_ref), nombre_particule(_nb), coef_simulation(_coef) 
+{/*METTRE PLEIN DE BAILS LA*/}
+
 
--- projet/programme/Faisceau.h
+++ projet/programme/Faisceau.h
@@ -1,1 +1,32 @@
+#pragma once
 
+#include <vector>
+
+#include "Dessinable.h"
+#include "SupportADesssin.h"
+
+#include "Particule.h"
+
+class Particule;
+
+class Faisceau : public Dessinable {
+	
+	private : 
+	
+	Particule reference;
+	long int nombre_particule;
+	double coef_simulation;
+	std::vector<Particule> CollectionParticule;
+	
+	double energie_moyenne;
+	//APRES LES EMITENCES (EN FAIRE 2?) + LES COEF D'ELLIPSE POUR CHACUNE D'ELLES (EN FAIRE 2?, FAIRE EN VECTOR?)
+	//CES DERNIERES + ENERGIE A CALCULER DANS LE {} DU CONSTRUCTEUR
+	
+	
+	
+	
+	public :
+	Faisceau(Particule,long int, double, SupportADessin* _support=nullptr);
+	
+	
+};
