c739cdd6eb4c28b85db13bbea1931f8b06c8b73d
josephine potdevin <josephinepotdevin@gmail.com>
2019-04-17 11:58:43 +0200

    intialisation de l'energie du faisceau

--- projet/programme/Faisceau.h
+++ projet/programme/Faisceau.h
@@ -13,12 +13,12 @@ class Faisceau : public Dessinable {
 	
 	private : 
 	
-	Particule reference;
+	Particule reference;//LA POSITION DU VECTEUR DE REF INDIQUE LE POINT D'INJECTION 
 	long int nombre_particule;
 	double coef_simulation;
 	std::vector<Particule> CollectionParticule;
 	
-	double energie_moyenne;
+	double energie_moyenne;//QUE FAIRE? ELLES ONT TOUTES MEME ENERGIE DONC JUSTE PRENDRE L'ENERGIE DE LA PARTICULE DE REF? OU ALORS IL FAUT PRENDRE ENERGIE DE LA PARTICULE AUGMENTÉE? OU FAIRE DES MOYENNES???
 	//APRES LES EMITENCES (EN FAIRE 2?) + LES COEF D'ELLIPSE POUR CHACUNE D'ELLES (EN FAIRE 2?, FAIRE EN VECTOR?)
 	//CES DERNIERES + ENERGIE A CALCULER DANS LE {} DU CONSTRUCTEUR
 	
